5588b959a4b3d282314347fbea103121b0c6cbc3
symbat <symbat.nurlankyzy8@gmail.com>
2026-02-03 16:31:55 +0500

    Assignment 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-        DatabaseManager.addInstitution("Eurasian National University", "Astana", 1996);
-        DatabaseManager.addInstitution("Turan University", "Astana", 1992);
-
-
-        DatabaseManager.addTeacher("Alua Myrzakul", "Database Management Systems");
-        DatabaseManager.addTeacher("Duman Marat", "Discrete Math");
-
-
-        DatabaseManager.addStudent("Bakdaulet", "Amankan", 18, 3);
-        DatabaseManager.addStudent("Imangali", "Abraim", 17, 1);
-
+        SpringApplication.run(Main.class, args);
     }
 }
